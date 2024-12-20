15da041ad49c1e90a2fdbbd79b335aa7e7e52055
gulsenurgunes <gulsenurgunes@gmail.com>
2024-12-20 13:48:11 +0300

    detection structure was created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.detekt)
 }
 
 android {
@@ -65,5 +66,15 @@ dependencies {
     //Lottie
     implementation(libs.lottie.compose)
 
+    //Detekt
+    detektPlugins(libs.detekt)
+}
+
+detekt {
+    config.setFrom(file("$rootDir/detekt/detektConfig.yml"))
+    source.from(files("src/main/kotlin"))
+    parallel = true
+    autoCorrect = true
+    buildUponDefaultConfig = true
 }
 
